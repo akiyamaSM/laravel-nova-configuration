eaa0646aa95fb0a792ca3358c382c30a51ca79e8
Inani El Houssain <inanielhoussain@gmail.com>
2018-09-24 17:41:31 +0100

    Get components

---
+++ resources/js/components/Configurations.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <div class="configuration" v-for="(configuration, index) in configurations">
+        {{ configuration.label }}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'list-configurations',
+    props: ['configurations']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/endpoint', function (Request $request) {
-//     //
-// });
+ Route::get('/getAllConfigurations', function () {
+        return \Inani\LaravelNovaConfiguration\Helpers\Configuration::all();
+ });
